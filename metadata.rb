740c87a7cfc855b737acee2f48eff724c7cbc991
Jennifer Davis <iennae@gmail.com>
2018-02-04 00:34:02 -0800

    Cleanup for the Supermarket, and linting issues
    * Added a kitchen.yml configuration to test platforms. Couldn't determine which platforms were
     supported to update metadata as both centos and ubuntu failed with the unit file. It'd be helpful
     to understand which version of chef you are targeting this to work for to understand what
     resources are usable.
    * Added the MIT license
    * Added headings to README for platform and chef information so that it's explicit about what is
     used. I don't know what should go here so I left them blank, but helpful to know what these are.
    * Cleaned up rubocop errors based off of cookstyle reports (a pinned rubocop) that's helpful across
    * community cookbooks to standardize on acceptable ruby conventions.
    * Cleaned up foodcritic errors. Remaining:
    
    FC066: Ensure chef_version is set in metadata: ./metadata.rb:1
    FC067: Ensure at least one platform supported in metadata: ./metadata.rb:1
    
    Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'MIT'
 description      'Install GoDNS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.3.4'
+issues_url 'https://github.com/Scalingo/godns-cookbook/issues'
+source_url 'https://github.com/Scalingo/godns-cookbook'
